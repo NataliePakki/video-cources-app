33d5d6f6ee0adcb94f0fba618cf5f8a74ae6ee5a
Natalie Pakki <natalie.pakki@gmail.com>
2018-07-19 21:37:11 +0300

    add tests to increase code coverage

--- src/app/pipes/order-by.pipe.spec.ts
+++ src/app/pipes/order-by.pipe.spec.ts
@@ -6,7 +6,7 @@ describe('OrderByPipe', () => {
   const firstCourseListItem = new CourseItem(1, 'abcd', 'Natalie', 'efg', 0, new Date(2018, 4, 4));
   const secondCourseListItem =  new CourseItem(2, 'ihl', 'Pakki', 'yzd', 0, new Date(2017, 4, 4));
 
-  const courseListItems = [
+  let courseListItems = [
     firstCourseListItem, secondCourseListItem
   ];
   it('create an instance', () => {
@@ -17,5 +17,18 @@ describe('OrderByPipe', () => {
   it('should orderBy date', () => {
     const actual = pipe.transform(courseListItems, 'creationDate');
     expect(actual).toEqual([ secondCourseListItem, firstCourseListItem]);
+  });
+
+  it('should orderBy date', () => {
+    courseListItems = [
+      secondCourseListItem, firstCourseListItem
+    ];
+    const actual = pipe.transform(courseListItems, 'creationDate');
+    expect(actual).toEqual([ secondCourseListItem, firstCourseListItem]);
+  });
+
+  it('should return courseList when orderBy not date', () => {
+    const actual = pipe.transform(courseListItems, 'another');
+    expect(actual).toEqual(courseListItems);
   });
 });
